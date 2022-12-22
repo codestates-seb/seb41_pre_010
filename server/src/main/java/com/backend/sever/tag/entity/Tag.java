b4215d8b70ae093b1191fea7711237d7ee9e8c3b
SungCheon Ju <jution113@gmail.com>
2022-12-22 13:53:32 +0900

    [BE MISC] Tag Entity 추가

--- server/src/main/java/com/backend/sever/tag/entity/Tag.java
+++ server/src/main/java/com/backend/sever/tag/entity/Tag.java
@@ -1,4 +1,20 @@
 package com.backend.sever.tag.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionTagId;
 }
