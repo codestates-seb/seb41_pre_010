e5f8f686b9b40eeb0d9e075c2e1d992b98412a68
HJeong1200 <hjeong1200@gmail.com>
2022-12-22 13:23:42 +0900

    [FE Style] Header 및 Footer 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
-import { BrowserRouter, Route, Routes , Link } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
 
 function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/questions" element={<MainPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/Pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Styles/MainPage.css";
+
+function MainPage() {
+  return (
+    <>
+      <header className="Header"></header>
+      <main className="MainPage_Container"></main>
+      <footer className="Footer"></footer>
+    </>
+  );
+}
+
+export default MainPage;
