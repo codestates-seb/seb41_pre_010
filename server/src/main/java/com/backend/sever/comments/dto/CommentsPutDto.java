693978921edf0c957698ee7ea38857f1063e8a1d
ppk150 <vytpdnd11@naver.com>
2022-12-22 23:06:54 +0900

    [BE FEAT] CommentsPut를 위한 CommentsPutDto 추가

--- server/src/main/java/com/backend/sever/comments/dto/CommentsPutDto.java
+++ server/src/main/java/com/backend/sever/comments/dto/CommentsPutDto.java
@@ -1,13 +1,17 @@
 package com.backend.sever.comments.dto;
 
 import com.backend.sever.comments.entity.Comments;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 
 @Getter
+@Setter
+@AllArgsConstructor
 public class CommentsPutDto {
 
-    private long commnetId;
+    private long commentsId;
 
     private String body;
 
