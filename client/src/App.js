c5cde605805a102e5cda4e06b185a31d81d50b19
limdumb <dlaruddls@nate.com>
2022-12-22 16:17:45 +0900

    [FE Style] Question Container 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import SignUp from './Pages/SignUp';
 import Login from './Pages/Login';
+import Question from './Pages/Question';
 import { BrowserRouter, Route, Routes , Link } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   <Routes>
     <Route path='/users/signup' element={<SignUp/>}/>
     <Route path='/users/islogin' element={<Login/>}/>
+    <Route path='/question' element={<Question/>}/>
   </Routes>
   </BrowserRouter>)
 }
---
+++ client/src/Pages/Question.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Styles/Question.css"
+
+export default function Question() {
+  return (
+    <>
+      <header className="Header"/>
+      <main>
+        <div className="Question_Container">
+          <div className="Question_Content_Container">
+          </div>
+        </div>
+      </main>
+      <footer className="Footer"/>
+    </>
+  );
+}
