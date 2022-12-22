5d00457ebb900960b1c802587759532982f1d042
HJeong1200 <hjeong1200@gmail.com>
2022-12-22 14:18:53 +0900

    [FE Style] Question Title 컨테이너 레이아웃 설정

--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -6,8 +6,17 @@ function MainPage() {
     <div className="MainPage_Container">
       <header className="Header"></header>
       <main className="Questions_Container">
-        <div className="Questions_Title_Container"></div>
-        <div className="Questions_Filter_Container"></div>
+        <div className="Questions_Title_Container">
+          <h1>모든 질문</h1>
+          <button>질문하기</button>
+        </div>
+        <div className="Questions_SubTitle_Container">
+          <h3>23,338,049 Questions</h3>
+          <div className="Questions_Filter_Container">
+            <button>최신순</button>
+            <button>추천순</button>
+          </div>
+        </div>
         <div className="Questions_List_Container">
           <div className="Question_Container"></div>
           <div className="Question_Container"></div>
