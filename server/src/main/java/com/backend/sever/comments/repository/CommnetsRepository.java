19235157314e32fb0c66eefb11b44690b3fb5cde
ppk150 <vytpdnd11@naver.com>
2022-12-22 19:48:45 +0900

    [BE MISC] CommentsRepository 추가

--- server/src/main/java/com/backend/sever/comments/repository/CommnetsRepository.java
+++ server/src/main/java/com/backend/sever/comments/repository/CommnetsRepository.java
@@ -4,5 +4,4 @@ import com.backend.sever.comments.entity.Comments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommnetsRepository extends JpaRepository<Comments , Long> {
-
 }
