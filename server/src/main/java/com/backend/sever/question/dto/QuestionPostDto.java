a04af6fd46f9c50f4312ed82c125fb093574859a
SungCheon Ju <jution113@gmail.com>
2022-12-23 00:01:46 +0900

    [BE Feat] QuestionPostDto에 tags 필드 추가

--- server/src/main/java/com/backend/sever/question/dto/QuestionPostDto.java
+++ server/src/main/java/com/backend/sever/question/dto/QuestionPostDto.java
@@ -1,8 +1,11 @@
 package com.backend.sever.question.dto;
 
+import com.backend.sever.tag.entity.Tag;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -10,4 +13,6 @@ import lombok.Setter;
 public class QuestionPostDto {
     private String title;
     private String body;
+    private List<Tag> tags;
 }
+g
