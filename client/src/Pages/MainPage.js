1bd17ae51e752d2b1142cd99a2ecfb8488702b15
HJeong1200 <hjeong1200@gmail.com>
2022-12-22 13:33:22 +0900

    [FE Style] 전체 레이아웃 Flex로 변경

--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -3,11 +3,16 @@ import "./Styles/MainPage.css";
 
 function MainPage() {
   return (
-    <>
+    <div className="MainPage_Container">
       <header className="Header"></header>
-      <main className="MainPage_Container"></main>
+      <main className="Questions_Container">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </main>
       <footer className="Footer"></footer>
-    </>
+    </div>
   );
 }
 
