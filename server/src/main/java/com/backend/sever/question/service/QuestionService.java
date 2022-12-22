cfab525acd3e87794f2cea1bc940fb995faf5961
SungCheon Ju <jution113@gmail.com>
2022-12-22 21:29:32 +0900

    [BE Feat] QuestionService 생성

---
+++ server/src/main/java/com/backend/sever/question/service/QuestionService.java
@@ -0,0 +1,11 @@
+package com.backend.sever.question.service;
+
+import com.backend.sever.question.entity.Question;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuestionService {
+    private Question createQuestion(Question question) {
+        return null;
+    }
+}
