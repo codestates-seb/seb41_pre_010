9067ca0876d41056a6b8b36132ecf95f09c49636
ppk150 <vytpdnd11@naver.com>
2022-12-22 19:49:21 +0900

    [BE MISC] CommentsPostDto 추가

---
+++ server/src/main/java/com/backend/sever/comments/dto/CommentsPostDto.java
@@ -0,0 +1,15 @@
+package com.backend.sever.comments.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentsPostDto {
+
+    private String body;
+}
