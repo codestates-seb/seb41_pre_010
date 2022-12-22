f03e5c357559478f596a0c53365dcbbdf2047ffa
SungCheon Ju <jution113@gmail.com>
2022-12-22 13:43:50 +0900

    [BE MISC] Comments Entity 추가

--- server/src/main/java/com/backend/sever/comments/entity/Comments.java
+++ server/src/main/java/com/backend/sever/comments/entity/Comments.java
@@ -1,4 +1,31 @@
 package com.backend.sever.comments.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Comments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentsId;
+
+    @Column(nullable = false)
+    private String body;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt = LocalDateTime.now();
 }
