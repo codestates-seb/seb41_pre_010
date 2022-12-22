e87326d8f7341e0f8345c4852628c17392bb476f
HJeong1200 <hjeong1200@gmail.com>
2022-12-22 16:47:06 +0900

    [FE Style] Edit 페이지 전체 레이아웃 설정

--- client/src/Pages/EditPage.js
+++ client/src/Pages/EditPage.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./Styles/EditPage.css";
 
 function EditPage() {
-  return <div>Edit Page</div>;
+  return (
+    <div className="EditPage_Container">
+      <header className="Header"></header>
+      <div className="Edit_Container">
+        <div className="Title_Container"></div>
+        <div className="Body_Container"></div>
+        <div className="Preview_Container"></div>
+        <div className="Tags_Container"></div>
+        <div className="Buttons_Container"></div>
+      </div>
+      <footer className="Footer"></footer>
+    </div>
+  );
 }
 
 export default EditPage;
