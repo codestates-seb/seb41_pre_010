a8a9cbaa926be58891d9f007e1a0ed7c21af2b81
HJeong1200 <hjeong1200@gmail.com>
2022-12-22 14:12:13 +0900

    [FE Style] Question List 컨테이너 레이아웃 설정

--- client/src/Pages/MainPage.js
+++ client/src/Pages/MainPage.js
@@ -6,10 +6,17 @@ function MainPage() {
     <div className="MainPage_Container">
       <header className="Header"></header>
       <main className="Questions_Container">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="Questions_Title_Container"></div>
+        <div className="Questions_Filter_Container"></div>
+        <div className="Questions_List_Container">
+          <div className="Question_Container"></div>
+          <div className="Question_Container"></div>
+          <div className="Question_Container"></div>
+          <div className="Question_Container"></div>
+          <div className="Question_Container"></div>
+          <div className="Question_Container"></div>
+        </div>
+        <div className="Questions_Pagination_Container"></div>
       </main>
       <footer className="Footer"></footer>
     </div>
