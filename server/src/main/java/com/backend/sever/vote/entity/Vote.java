a4d9931e8a7e11077593bf9f455b56a15a8bc8ed
SungCheon Ju <jution113@gmail.com>
2022-12-22 13:56:45 +0900

    [BE MISC] Vote Entity 추가

--- server/src/main/java/com/backend/sever/vote/entity/Vote.java
+++ server/src/main/java/com/backend/sever/vote/entity/Vote.java
@@ -1,4 +1,20 @@
 package com.backend.sever.vote.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+
+    @Column(nullable = false)
+    private int voteCount;
 }
