146d469ba3cfb645e8e652ebcd26c8771baaa203
SungCheon Ju <jution113@gmail.com>
2022-12-22 21:50:32 +0900

    [BE Feat] QuestionService에 question 생성 기능 구현

--- server/src/main/java/com/backend/sever/question/service/QuestionService.java
+++ server/src/main/java/com/backend/sever/question/service/QuestionService.java
@@ -1,11 +1,16 @@
 package com.backend.sever.question.service;
 
 import com.backend.sever.question.entity.Question;
+import com.backend.sever.question.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
+    private final QuestionRepository questionRepository;
+    public QuestionService(QuestionRepository questionRepository) {
+        this.questionRepository = questionRepository;
+    }
     private Question createQuestion(Question question) {
-        return null;
+        return questionRepository.save(question);
     }
 }
