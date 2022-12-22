9c3dad8d70f7fcb0604fa0fa40f3a3e09b8dfdee
sktjgudals <sktjgudals@naver.com>
2022-12-22 16:19:01 +0900

    [FE MISC] App.js  설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 import { BrowserRouter, Route, Routes , Link } from 'react-router-dom';
-
+import Mypage from './Pages/Mypage'
 function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/users/mypage"} element={<Mypage/> } />
+      </Routes>
+      </BrowserRouter>
+
+  )
 }
 
 export default App;
