03c05aa29bfd26a7a2b4f7cd810b4abed598e90b
HanSungHyeon <hsh5349@naver.com>
2022-12-23 10:32:30 +0900

    [BE FEAT] findUser 서비스로직 생성

---
+++ server/src/main/java/com/backend/sever/user/service/UserService.java
@@ -0,0 +1,21 @@
+package com.backend.sever.user.service;
+
+import com.backend.sever.user.entity.User;
+import com.backend.sever.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findUser(long userId) {
+        //스텁으로 진행
+        User stubUser = new User("이미지url","유저 닉네임","유저 email","유저 password");
+
+        return stubUser;
+    }
+}
