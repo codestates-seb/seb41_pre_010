c1dd6a5602e3bf35870d7fe3a6db25edcb537231
limdumb <dlaruddls@nate.com>
2022-12-23 15:36:51 +0900

    [FE Feat] Tag Component 분리

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,27 @@ import Login from "./Pages/Login";
 import MainPage from "./Pages/MainPage";
 import Mypage from "./Pages/Mypage";
 import MypageEdit from "./Pages/MypageEdit";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import EditPage from "./Pages/EditPage";
+import Footer from "./Components/Footer";
+// import Header from "./Components/Header";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/question/:questionId" element={<Question />} />
-        <Route path="/questions" element={<MainPage />} />
-        <Route path="/users/signup" element={<SignUp />} />
-        <Route path="/users/islogin" element={<Login />} />
-        <Route path="/questions/:questionId/edit" element={<EditPage />} />
-        <Route path={"/users/mypage/:userId"} element={<Mypage />} />
-        <Route path={"/users/mypage/edit/:userId"} element={<MypageEdit />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/question/:questionId" element={<Question />} />
+          <Route path="/questions" element={<MainPage />} />
+          <Route path="/users/signup" element={<SignUp />} />
+          <Route path="/users/islogin" element={<Login />} />
+          <Route path="/questions/:questionId/edit" element={<EditPage />} />
+          <Route path={"/users/mypage/:userId"} element={<Mypage />} />
+          <Route path={"/users/mypage/edit/:userId"} element={<MypageEdit />} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
 
---
+++ client/src/Components/Tag.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Styles/Tag.css"
+
+export default function Tag() {
+  return (
+      <a href="" className="Tag_Button">props예정</a>
+  );
+}
