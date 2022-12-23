27ce7c8043bf71d19f02e574db69936c52964112
HJeong1200 <hjeong1200@gmail.com>
2022-12-23 11:56:41 +0900

    [FE Style] White Button 컴포넌트 생성

---
+++ client/src/Components/button.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const WhiteButton = styled.button`
+  margin: 5px; // to be deleted!!!!!!!
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.font};
+  border: 1px solid rgb(234, 236, 237);
+  border-radius: 3px;
+  background-color: white;
+  cursor: pointer;
+  box-shadow: rgba(255, 255, 255, 0.2) 0px 1px 0px 0px inset;
+  &:hover {
+    background-color: rgb(214, 217, 220);
+    border-color: rgb(186, 191, 196);
+  }
+`;
+
+export default WhiteButton;
