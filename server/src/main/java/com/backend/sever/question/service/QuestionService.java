28d685c20310b5c468d45a0f937f974f3775e9da
SungCheon Ju <jution113@gmail.com>
2022-12-23 14:07:04 +0900

    [BE Feat] QuestionService에 findQuestion 기능 추가

--- server/src/main/java/com/backend/sever/question/service/QuestionService.java
+++ server/src/main/java/com/backend/sever/question/service/QuestionService.java
@@ -4,6 +4,8 @@ import com.backend.sever.question.entity.Question;
 import com.backend.sever.question.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
@@ -12,5 +14,14 @@ public class QuestionService {
     }
     public Question createQuestion(Question question) {
         return questionRepository.save(question);
+    }
+
+    public Question findQuestion(long questionId) {
+        Optional optionalQuestion = questionRepository.findById(questionId);
+        return verifyQuestion(optionalQuestion);
+    }
+
+    private Question verifyQuestion(Optional<Question> optionalQuestion) {
+        return optionalQuestion.orElseThrow(() -> new RuntimeException());
     }
 }
