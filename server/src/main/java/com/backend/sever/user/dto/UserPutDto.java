f3cc9604979079f58132f87fc91ec9d1bbcd0bff
HanSungHyeon <hsh5349@naver.com>
2022-12-23 11:24:34 +0900

    [BE MISC] putDto setter 추가

--- server/src/main/java/com/backend/sever/user/dto/UserPutDto.java
+++ server/src/main/java/com/backend/sever/user/dto/UserPutDto.java
@@ -2,8 +2,10 @@ package com.backend.sever.user.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 public class UserPutDto {
     private String displayName;
