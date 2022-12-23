23fe848d58e8f105909c31005da43eaa85c09574
sktjgudals <sktjgudals@naver.com>
2022-12-23 11:57:35 +0900

    [FE Style] Header 레이아웃 분리

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
   const [isLogin, setIsLogin] = useState(false);
   return (
     <header className="MainHeader">
-      <div className="MainHeader_Logo_Container"></div>
+      <div className="MainHeader_Logo_Container">
+        <img src={"image/logo-stackoverflow.png"} width={150} height={47} />
+      </div>
       <div className="MainHeader_Input_Container"> </div>
       {isLogin ? (
         <div className="MainHeader_UserInfo_Container">user</div>
