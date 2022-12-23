ac44649a9049a05e0dcaa40f2357ae283e423e4b
HanSungHyeon <hsh5349@naver.com>
2022-12-24 05:34:38 +0900

    [BE FEAT] question 일부 정보 수동 매핑

--- server/src/main/java/com/backend/sever/user/mapper/UserMapper.java
+++ server/src/main/java/com/backend/sever/user/mapper/UserMapper.java
@@ -1,13 +1,35 @@
 package com.backend.sever.user.mapper;
 
+import com.backend.sever.question.entity.Question;
+import com.backend.sever.user.dto.UserInfoQuestionListDto;
+import com.backend.sever.user.dto.UserInfoResponseDto;
 import com.backend.sever.user.dto.UserPutDto;
 import com.backend.sever.user.dto.UserResponseDto;
 import com.backend.sever.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserResponseDto userToUserResponseDto(User user);
 
     User userPutToDtoToUser(UserPutDto userPutDto);
+
+    default UserInfoQuestionListDto questionToUserInfoQuestionListDto(Question question) {
+        UserInfoQuestionListDto.UserInfoQuestionListDtoBuilder userInfoQuestionListDtoBuilder = UserInfoQuestionListDto.builder();
+        userInfoQuestionListDtoBuilder.vote(question.getVotes().size());
+        userInfoQuestionListDtoBuilder.questionId(question.getQuestionId());
+        userInfoQuestionListDtoBuilder.title(question.getTitle());
+        userInfoQuestionListDtoBuilder.createdAt(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(question.getCreatedAt()));
+        userInfoQuestionListDtoBuilder.modifiedAt(DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(question.getModifiedAt()));
+
+        return userInfoQuestionListDtoBuilder.build();
+    }
+
+    UserInfoResponseDto userToUserInfoResponseDto(User user);
+
+    List<UserInfoResponseDto> questionToUserInfoResponseDtoList(List<Question> questions);
 }
