e5b70c219e347cfd21c906dc041ea6c1996f2331
SungCheon Ju <jution113@gmail.com>
2022-12-25 14:59:17 +0900

    [BE Refactor] TagRepository에 불필요 import 삭제

--- server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
+++ server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
@@ -2,7 +2,6 @@ package com.backend.sever.tag.repository;
 
 import com.backend.sever.tag.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
