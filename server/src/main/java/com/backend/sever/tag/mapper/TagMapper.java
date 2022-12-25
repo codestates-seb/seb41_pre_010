ba6a5572b1c99c975fa080de9b71b6196fa5053b
SungCheon Ju <jution113@gmail.com>
2022-12-26 00:12:47 +0900

    [BE Feat] TagMpper 생성

--- server/src/main/java/com/backend/sever/tag/mapper/TagMapper.java
+++ server/src/main/java/com/backend/sever/tag/mapper/TagMapper.java
@@ -1,2 +1,12 @@
-package com.backend.sever.tag.mapper;public interface TagMapper {
+package com.backend.sever.tag.mapper;
+
+import com.backend.sever.tag.dto.TagPostDto;
+import com.backend.sever.tag.dto.TagResponseDto;
+import com.backend.sever.tag.entity.Tag;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface TagMapper {
+    Tag tagPostDtoToTag(TagPostDto tagPostDto);
+    TagResponseDto tagToTagResponseDto(Tag tag);
 }
