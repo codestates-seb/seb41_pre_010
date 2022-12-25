40b4218b4498e66f855a4428688b20318b8caec4
ppk150 <vytpdnd11@naver.com>
2022-12-25 21:59:33 +0900

    [BE FEAT] UserService jwt 를 위한 기능 추가

--- server/src/main/java/com/backend/sever/user/service/UserService.java
+++ server/src/main/java/com/backend/sever/user/service/UserService.java
@@ -1,9 +1,15 @@
 package com.backend.sever.user.service;
 
 import com.backend.sever.config.CustomBeanUtils;
+import com.backend.sever.jwt.utils.CustomAuthorityUtils;
 import com.backend.sever.user.entity.User;
 import com.backend.sever.user.repository.UserRepository;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+// 사용자 로그인을 위한 요소 임포트
+
 
 import java.util.Optional;
 
@@ -12,10 +18,17 @@ public class UserService {
     private final UserRepository userRepository;
     private final CustomBeanUtils<User> beanUtils;
 
+    // 사용자 로그인을 위한 클래스 DI
+
+
+
+
     public UserService(UserRepository userRepository, CustomBeanUtils<User> beanUtils) {
         this.userRepository = userRepository;
         this.beanUtils = beanUtils;
     }
+
+
 
     public User findUser(long userId) {
         User findUser = verifiedUser(userId);
