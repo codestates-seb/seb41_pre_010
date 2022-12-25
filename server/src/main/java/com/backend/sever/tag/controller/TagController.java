edc1fe94abeb89372d6a6d45c2aeefb80b2cb486
SungCheon Ju <jution113@gmail.com>
2022-12-26 00:32:49 +0900

    [BE Feat] TagController 생성 및 postTag 구현

--- server/src/main/java/com/backend/sever/tag/controller/TagController.java
+++ server/src/main/java/com/backend/sever/tag/controller/TagController.java
@@ -1,2 +1,31 @@
-package com.backend.sever.tag.controller;public class TagController {
+package com.backend.sever.tag.controller;
+
+import com.backend.sever.tag.dto.TagPostDto;
+import com.backend.sever.tag.entity.Tag;
+import com.backend.sever.tag.mapper.TagMapper;
+import com.backend.sever.tag.service.TagService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/tags")
+public class TagController {
+    private final TagMapper mapper;
+    private final TagService tagService;
+
+    public TagController(TagMapper mapper, TagService tagService) {
+        this.mapper = mapper;
+        this.tagService = tagService;
+    }
+
+    @PostMapping
+    public ResponseEntity postTag(@RequestBody TagPostDto tagPostDto) {
+        Tag tag = tagService.createTag(mapper.tagPostDtoToTag(tagPostDto));
+
+        return new ResponseEntity<> (mapper.tagToTagResponseDto(tag), HttpStatus.CREATED);
+    }
 }
