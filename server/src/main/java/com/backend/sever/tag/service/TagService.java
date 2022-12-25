a1e57c1d9bc9fae3a371dbe26018421325db1a98
SungCheon Ju <jution113@gmail.com>
2022-12-25 15:20:42 +0900

    [BE Refactor] TagService - tag 중복 등록 검사 로직 추가

--- server/src/main/java/com/backend/sever/tag/service/TagService.java
+++ server/src/main/java/com/backend/sever/tag/service/TagService.java
@@ -4,6 +4,8 @@ import com.backend.sever.tag.entity.Tag;
 import com.backend.sever.tag.repository.TagRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TagService {
     private TagRepository tagRepository;
@@ -13,7 +15,21 @@ public class TagService {
     }
 
     public Tag createService(Tag tag) {
+        verifyExistTag(tag);
 
         return tagRepository.save(tag);
     }
+
+    private void verifyExistTag(Tag tag) {
+        Optional<Tag> optionalTag = tagRepository.findById(tag.getTagId());
+        if(optionalTag.isPresent()) {
+            // 이미 등록된 Tag를 등록하려 한다면 RuntimeError 발생
+            try {
+                throw new RuntimeException("이미 존재하는 Tag 입니다.");
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
 }
