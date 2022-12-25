fd317b2bb8c95971be6fb57d4e8a6dbc78800f31
ppk150 <vytpdnd11@naver.com>
2022-12-25 21:53:52 +0900

    [BE FEAT] SecurityConfiguration 추가

--- server/src/main/java/com/backend/sever/jwt/configration/SecurityConfiguration.java
+++ server/src/main/java/com/backend/sever/jwt/configration/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.backend.sever.jwt.configration;
 
+import com.backend.sever.jwt.filter.JwtAuthenticationFilter;
+import com.backend.sever.jwt.token.JwtTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,6 +20,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private  final JwtTokenizer jwtTokenizer;
+
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
@@ -25,6 +36,8 @@ public class SecurityConfiguration {
                 .cors(withDefaults())
                 .formLogin().disable()
                 .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll()
                 );
 
@@ -45,6 +58,19 @@ public class SecurityConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**",configuration);
         return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer <CustomFilterConfigurer , HttpSecurity>{
+        @Override
+        public void configure(HttpSecurity builder) throws  Exception{
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter =new JwtAuthenticationFilter(authenticationManager,jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("api/v1/users/login");
+
+            builder.addFilter(jwtAuthenticationFilter);
+        }
+
     }
 
 }
