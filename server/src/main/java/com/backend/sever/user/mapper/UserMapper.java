e707be481a89844d4e7ed39ef0aa16457690d0bf
ppk150 <vytpdnd11@naver.com>
2022-12-25 22:26:37 +0900

    [BE FEAT] UserPost 기능 추가를 위한 userPostToUser 추가

--- server/src/main/java/com/backend/sever/user/mapper/UserMapper.java
+++ server/src/main/java/com/backend/sever/user/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.backend.sever.user.mapper;
 
+import com.backend.sever.user.dto.UserPostDto;
 import com.backend.sever.user.dto.UserPutDto;
 import com.backend.sever.user.dto.UserResponseDto;
 import com.backend.sever.user.entity.User;
@@ -10,4 +11,7 @@ public interface UserMapper {
     UserResponseDto userToUserResponseDto(User user);
 
     User userPutToDtoToUser(UserPutDto userPutDto);
+
+    // 회원 가입을 위해 추가
+    User userPostToUser (UserPostDto userPostDto);
 }
