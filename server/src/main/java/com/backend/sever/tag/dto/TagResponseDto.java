2bd1b01dab3dd38723cf263224d484866ca2b705
SungCheon Ju <jution113@gmail.com>
2022-12-26 00:14:02 +0900

    [BE Feat] TagResponseDto 생성

--- server/src/main/java/com/backend/sever/tag/dto/TagResponseDto.java
+++ server/src/main/java/com/backend/sever/tag/dto/TagResponseDto.java
@@ -10,11 +10,4 @@ import lombok.Setter;
 public class TagResponseDto {
     private long tagId;
     private String tagName;
-
-    // --- Stub 데이터 생성용 생성자 ---
-    public TagResponseDto(long tagId, String tagName) {
-        this.tagId = tagId;
-        this.tagName = tagName;
-    }
-    // ---
 }
