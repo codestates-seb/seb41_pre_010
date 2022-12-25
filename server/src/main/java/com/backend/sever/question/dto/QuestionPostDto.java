9b30a03a78013b6549a782ddd0eb73246d7a2302
SungCheon Ju <jution113@gmail.com>
2022-12-26 00:35:37 +0900

    [BE Refactor] QuestionPostDto tags 타입 변경 QuestionPostDto -> QuestionTag

--- server/src/main/java/com/backend/sever/question/dto/QuestionPostDto.java
+++ server/src/main/java/com/backend/sever/question/dto/QuestionPostDto.java
@@ -1,6 +1,6 @@
 package com.backend.sever.question.dto;
 
-import com.backend.sever.tag.dto.TagPostDto;
+import com.backend.sever.questionTag.dto.QuestionTagDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,5 +13,5 @@ import java.util.List;
 public class QuestionPostDto {
     private String title;
     private String body;
-    private List<TagPostDto> tags;
+    private List<QuestionTagDto> tags;
 }
