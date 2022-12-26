577ef864f826eabda97a8b8db83135f5fd654391
limdumb <dlaruddls@nate.com>
2022-12-26 09:08:17 +0900

    [FE Feat] Tag Component 구현

--- client/src/Components/Tag.jsx
+++ client/src/Components/Tag.jsx
@@ -1,8 +1,27 @@
-import React from "react";
-import "./Styles/Tag.css"
+import React, { useState } from "react";
+import { TagsInput } from "react-tag-input-component";
+import styled from "styled-components";
+import "./Styles/style.css";
 
-export default function Tag() {
+export const TagButton = styled.a `
+  font-size: 12px;
+  color: hsl(205,47%,42%);
+  background-color: hsl(205,46%,92%);
+  border-radius: 4px;
+  border: none;
+  padding: 4px 6px;
+  text-decoration-line: none;
+`
+export const TagBar = () => {
+  const [selected, setSelected] = useState([]);
   return (
-      <a href="" className="Tag_Button">props예정</a>
+    <div className="Tag_Components">
+      <TagsInput
+        value={selected}
+        onChange={setSelected}
+        name="fruits"
+        placeHolder="태그를 입력하세요"
+      />
+    </div>
   );
 }
--- client/src/Pages/Question.jsx
+++ client/src/Pages/Question.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import "./Styles/Question.css";
+import { TagButton } from "../Components/Tag";
 
 const StyledSpan = styled.span`
   font-size: ${(props) => props.fontsize};
@@ -42,7 +43,7 @@ export default function Question() {
               </div>
             </div>
             <div className="Tag_Section">
-              <button className="Tag_Button">React</button>
+              <TagButton>React</TagButton>
             </div>
             {/* 상태값에 따른 조건부 설정 예정 */}
             <div className="Contour_Line" />
