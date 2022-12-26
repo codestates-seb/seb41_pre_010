83aa412986c4ac3eac6ee39143a2b040de4efdb7
limdumb <dlaruddls@nate.com>
2022-12-26 14:38:44 +0900

    [FE Refact] Question Data교체 진행

--- client/src/Pages/Question.jsx
+++ client/src/Pages/Question.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import "./Styles/Question.css";
 import { TagButton } from "../Components/Tag";
+import { questionDummyData } from "../QuestionData";
 
 const StyledSpan = styled.span`
   font-size: ${(props) => props.fontsize};
   color: ${(props) => props.color};
 `;
 
+//0번 데이터의 기준으로 작성 추후 데이터ID를 받을 예정
 export default function Question() {
+  //추후 useState기본값 null처리
+  const [questionData, setQeustionData] = useState(questionDummyData);
+  const [commentClick, setCommentClic] = useState(false);
+
+  const commentToggle = () => {
+    setCommentClic(!commentClick);
+  };
+  //실제 API 정보에서 수정 예정
+  // useEffect(() => {
+  //   const getQuestionData = async () => {
+  //     const result = await 데이터함수(Click시 넘어온 ID값)
+  //      setQuestionData(result)
+  //   };
+
+  //   getQuestionData()
+  // },[]);
+  const filterData = questionData;
+  console.log(commentClick);
   return (
     <>
       <main>
@@ -16,77 +36,123 @@ export default function Question() {
           <div className="Question_Content_Container">
             <div className="Title_Container">
               <div className="Title_Contents">
-                <h1>
-                  Web Scraper : Using Find All (Beautiful Soap, Python) and show
-                  result in Streamlit
-                </h1>
+                <h1>{filterData[0].title}</h1>
               </div>
               <div className="Ask_Question_Button_Container">
                 <button className="Ask_Question_Button">Ask Question</button>
                 <div className="Wright_Data_Info">
                   <StyledSpan fontsize={"14px"}>
-                    작성: 2022/12/22/14:21
+                    작성: {filterData[0].createdAt}
                   </StyledSpan>
                   <br></br>
-                  <StyledSpan fontsize={"14px"}>
-                    수정: 2022/12/23/14:52
-                  </StyledSpan>
-                  <div className="Modify_Icon">✍🏻</div>
+                  <div className="Modify_Data_Info">
+                    <StyledSpan fontsize={"14px"}>
+                      수정: {filterData[0].modifiedAt}
+                    </StyledSpan>
+                    <div className="Modify_Icon">✍🏻</div>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="Main_Text_Container">
-              <aside className="Main_Text_Aside">아이콘자리</aside>
+              <aside className="Main_Text_Aside">
+                {/* 임시 아이콘 */}
+                <div className="Vote_Icon_Container">
+                  <span>⬆</span>
+                  <span>3</span>
+                  <span>⬇</span>
+                </div>
+              </aside>
               <div className="Main_Text_Content">
-                <span>본문자리</span>
+                <span>{filterData[0].body}</span>
               </div>
             </div>
             <div className="Tag_Section">
-              <TagButton>React</TagButton>
+              {filterData &&
+                filterData.map((el) => {
+                  return el.tags.map((tags, index) => {
+                    return (
+                      <div key={index} className="Tags_Element">
+                        <TagButton>{tags.tagName}</TagButton>
+                      </div>
+                    );
+                  });
+                })}
+            </div>
+            <div className="Question_User_Profil_Container">
+              <div className="Question_User_Profil">
+                <img
+                  className="Question_User_Image"
+                  src={`${filterData[0].user.profileImage}`}
+                />
+                <span>{`${filterData[0].user.displayName}`}</span>
+              </div>
             </div>
             {/* 상태값에 따른 조건부 설정 예정 */}
-            <div className="Contour_Line" />
+            {filterData[0].answers ? <div className="Contour_Line" /> : null}
             {/* 질문 여부에 따른 조건부 설정 예정 */}
-            <div className="Answers_Container">
-              <h2>Answer</h2>
-              <div className="Main_Text_Container">
-                <aside className="Main_Text_Aside">아이콘자리</aside>
-                <div className="Main_Text_Content">
-                  <span>본문자리</span>
-                </div>
-              </div>
-              <div className="Answer_User_Profil_Container">
-                <div className="Answer_Contents">
-                  <div className="Answer_CreateAt">
-                    <span>2022/12/22/14:50</span>
-                    <span>✍🏻</span>
-                  </div>
-                  <div className="Answer_User_Profil">
-                    <img
-                      className="Answer_User_Image"
-                      src="https://pbs.twimg.com/profile_images/1497881718971273220/1ANhWWRq_400x400.jpg"
-                    />
-                    <span>김태연</span>
-                  </div>
-                </div>
-                {/* <div className="Answer_ModifyAt"></div> 조건부 랜더링예정 */}
-              </div>
-              <div className="Comment_Container">
-                <div className="Comment_Contour_Line">
-                  <span>코멘트 내용입니다</span>
-                </div>
-              </div>
-              <div className="Add_Comment">
-                <button className="Comment_Add_Button">Add a Comment</button>
-              </div>
-              {/* 상태값에 따른 조건부 설정 예정 */}
-              <div className="Contour_Line" />
-              <div className="Your_Answer_Container">
-                <h2>Your Answer</h2>
-                <div>Answer작성창 부분</div>
-                <div className="Submit_Clear_Container">
-                  <button>Post Yout Answer</button>
-                </div>
+            {filterData[0].answers
+              ? filterData &&
+                filterData[0].answers.map((el) => {
+                  return (
+                    <div key={el.answerId} className="Answers_Container">
+                      <h2>Answer{el.answerId}</h2>
+                      <div>
+                        <div className="Main_Text_Container">
+                          <aside className="Main_Text_Aside">
+                            <div className="Vote_Icon_Container">
+                              <span>⬆</span>
+                              <span>3</span>
+                              <span>⬇</span>
+                            </div>
+                          </aside>
+                          <div className="Main_Text_Content">
+                            <span>{el.body}</span>
+                          </div>
+                        </div>
+                        <div className="Answer_User_Profil_Container">
+                          <div className="Answer_Contents">
+                            <div className="Answer_CreateAt">
+                              <span>{el.createdAt}</span>
+                              <span>✍🏻</span>
+                            </div>
+                            <div>
+                              <img
+                                className="Answer_User_Image"
+                                src={`${el.user.profileImage}`}
+                              />
+                              <span>{el.user.displayName}</span>
+                            </div>
+                          </div>
+                          {/* <div className="Answer_ModifyAt"></div> 조건부 랜더링예정 */}
+                        </div>
+                        <div className="Comment_Container">
+                          <div className="Comment_Contour_Line">
+                            <span>코멘트 내용입니다</span>
+                          </div>
+                        </div>
+                        <div className="Add_Comment">
+                          {/* 추후 TextBox예정 */}
+                          {commentClick ? null : (
+                            <button
+                              className="Comment_Add_Button"
+                              onClick={commentToggle}
+                            >
+                              Add a Comment
+                            </button>
+                          )}
+                        </div>
+                        <div className="Contour_Line" />
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+            <div className="Your_Answer_Container">
+              <h2>Your Answer</h2>
+              <div>Answer작성창 부분</div>
+              <div className="Submit_Clear_Container">
+                <button>Post Yout Answer</button>
               </div>
             </div>
           </div>
---
+++ client/src/QuestionData.js
@@ -0,0 +1,56 @@
+export const questionDummyData = [
+  {
+    questionId: 0, //Number
+    user: {
+      userId: 77, //Number
+      displayName: "김태연", //String
+      profileImage: "https://pbs.twimg.com/profile_images/1497881718971273220/1ANhWWRq_400x400.jpg", //String
+    },
+    title: "개발 어케해요? 진짜 개어려움; ㄹㅇㅋㅋ", //String
+    body: "나 이렇게 취업이 가능한지 궁금합니다 리엑트 개어려워요 JS는 더더욱이 더 어렵구요 이거 ㄹㅇ 취업됩니까?ㅋㅋ;; 안될꺼같아서 문의 드려요 제발 살려주사뮤ㅠㅠ", //String
+    createdAt: "2022/12/26/11:53", //String
+    modifiedAt: "2022/12/27/12:52", //String
+    vote: 7, //Number
+    voteCheck: false, //Boolean
+    bookMarkCheck: false, //Boolean
+    tags: [
+      {
+        tagName: "React", //String
+      },
+      {
+        tagName: "Node.js", //String
+      },
+      {
+        tagName: "JavaScript", //String
+      }
+    ],
+    answers: [
+      {
+        answerId: 1, //Number
+        user: {
+          userId: 245, //Number
+          displayName: "유병재", //String
+          profileImage: "https://yt3.ggpht.com/ytc/AMLnZu-nsGDNtj2Kn2SthH20NUPJLILrahbH725dTyCGBw=s900-c-k-c0x00ffffff-no-rj", //String
+        },
+        comments: [
+          {
+            commentId: 0, //Number
+            user: {
+              userId: 255, //Number
+              displayName: "유리", //String
+            },
+            body: "ㄹㅇ취업은 결국 하기나름임", //String
+            createdAt: "2022/12/26/11:54", //String
+            modifiedAt: "2022/12/26/11:55", //String
+          },
+        ],
+        body: "취업은 하기 나름인듯 님이 얼마나 노력을 했는지에 따라 ㅋㅋ", //String
+        createdAt: "2022/12/26/11:53", //String
+        modifiedAt: "2022/12/26/11:55", //String
+        vote: 4, //Number
+        voteCheck: false, //Boolean
+        bookMarkCheck: false, //Boolean
+      },
+    ],
+  },
+];
