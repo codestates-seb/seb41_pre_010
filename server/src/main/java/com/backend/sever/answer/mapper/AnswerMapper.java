bbd540c6090d1b554dbd4e158890baa6b30bcea1
HanSungHyeon <hsh5349@naver.com>
2022-12-26 15:40:54 +0900

    [BE Refactor] 유저id 질문id 매핑

--- server/src/main/java/com/backend/sever/answer/mapper/AnswerMapper.java
+++ server/src/main/java/com/backend/sever/answer/mapper/AnswerMapper.java
@@ -4,11 +4,29 @@ import com.backend.sever.answer.dto.AnswerPostDto;
 import com.backend.sever.answer.dto.AnswerPutDto;
 import com.backend.sever.answer.dto.AnswerResponseDto;
 import com.backend.sever.answer.entity.Answer;
+import com.backend.sever.question.entity.Question;
+import com.backend.sever.user.entity.User;
 import org.mapstruct.Mapper;
+
+import java.time.LocalDateTime;
 
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
-    Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto);
+    default Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto){
+        Answer answer = new Answer();
+        User user = new User();
+        Question question = new Question();
+
+        user.setUserId(answerPostDto.getUserId());
+        question.setQuestionId(answerPostDto.getQuestionId());
+
+        answer.setBody(answerPostDto.getBody());
+        answer.setUser(user);
+        answer.setQuestion(question);
+        answer.setCreatedAt(LocalDateTime.now());
+
+        return answer;
+    }
     Answer answerPutDtoAnswer(AnswerPutDto answerPutDto);
     AnswerResponseDto answerToAnswerResponseDto(Answer answer);
 }
