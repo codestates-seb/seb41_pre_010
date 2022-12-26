d8a11d968f1525ced2c87a0ddbf959e83b6e4100
HanSungHyeon <hsh5349@naver.com>
2022-12-26 16:03:40 +0900

    [BE Refactor] 답변 매핑코드 전체 리팩토링

--- server/src/main/java/com/backend/sever/answer/mapper/AnswerMapper.java
+++ server/src/main/java/com/backend/sever/answer/mapper/AnswerMapper.java
@@ -4,29 +4,16 @@ import com.backend.sever.answer.dto.AnswerPostDto;
 import com.backend.sever.answer.dto.AnswerPutDto;
 import com.backend.sever.answer.dto.AnswerResponseDto;
 import com.backend.sever.answer.entity.Answer;
-import com.backend.sever.question.entity.Question;
-import com.backend.sever.user.entity.User;
 import org.mapstruct.Mapper;
-
-import java.time.LocalDateTime;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AnswerMapper {
-    default Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto){
-        Answer answer = new Answer();
-        User user = new User();
-        Question question = new Question();
-
-        user.setUserId(answerPostDto.getUserId());
-        question.setQuestionId(answerPostDto.getQuestionId());
-
-        answer.setBody(answerPostDto.getBody());
-        answer.setUser(user);
-        answer.setQuestion(question);
-        answer.setCreatedAt(LocalDateTime.now());
-
-        return answer;
-    }
+    @Mapping(source = "questionId",target = "question.questionId")
+    @Mapping(source = "userId", target = "user.userId")
+    Answer answerPostDtoToAnswer(AnswerPostDto answerPostDto);
+    @Mapping(source = "questionId",target = "question.questionId")
+    @Mapping(source = "userId", target = "user.userId")
     Answer answerPutDtoAnswer(AnswerPutDto answerPutDto);
     AnswerResponseDto answerToAnswerResponseDto(Answer answer);
 }
