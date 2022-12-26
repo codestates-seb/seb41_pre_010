5a0c1322ba0df86b07cc7611ed57423e533027dc
SungCheon Ju <jution113@gmail.com>
2022-12-26 17:43:57 +0900

    [BE Feat] TagPostDtos 추가

---
+++ server/src/main/java/com/backend/sever/tag/dto/TagPostDtos.java
@@ -0,0 +1,14 @@
+package com.backend.sever.tag.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class TagPostDtos {
+    private List<TagPostDto> tags;
+}
