bd05056a7df4fdc2572667878b1e7c3b85c52f94
HanSungHyeon <hsh5349@naver.com>
2022-12-27 01:05:39 +0900

    [BE FEAT] voteDto 하나로 통일

---
+++ server/src/main/java/com/backend/sever/vote/dto/VotePostDto.java
@@ -0,0 +1,14 @@
+package com.backend.sever.vote.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VotePostDto {
+    private int vote;
+}
