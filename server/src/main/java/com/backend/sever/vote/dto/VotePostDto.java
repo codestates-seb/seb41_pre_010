181fdcf6a7f1251927f3a57945081bfcff0b201f
HanSungHyeon <hsh5349@naver.com>
2022-12-27 02:46:22 +0900

    [BE FEAT] 추천 버튼 입력시 request

--- server/src/main/java/com/backend/sever/vote/dto/VotePostDto.java
+++ server/src/main/java/com/backend/sever/vote/dto/VotePostDto.java
@@ -2,13 +2,13 @@ package com.backend.sever.vote.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class VotePostDto {
     private int vote;
+    private long questionId;
+    private long userId;
 }
