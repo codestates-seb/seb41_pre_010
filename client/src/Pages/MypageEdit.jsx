8dca41ceadc6f746d768306d9c111e19a852a0b5
HJeong1200 <hjeong1200@gmail.com>
2022-12-27 14:15:17 +0900

    [FE Feat] 사용자 프로필 변경 기능

--- client/src/Pages/MypageEdit.jsx
+++ client/src/Pages/MypageEdit.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import Input from "../Components/Input";
 import { BlueButton, BlueWhiteButton } from "../Components/Button";
 import "./Styles/MypageEdit.css";
@@ -49,6 +50,32 @@ const UserProfile = () => {
 };
 
 const UserInfoEdit = () => {
+  const [newName, setNewName] = useState(dummyDataProfile.displayName);
+  const [newTitle, setNewTitle] = useState(dummyDataProfile.title);
+
+  function changeDisplayName(e) {
+    setNewName(e.target.value);
+  }
+
+  function changeTitle(e) {
+    setNewTitle(e.target.value);
+  }
+
+  function changeUserProfile() {
+    const newProfile = {
+      displayName: newName,
+      title: newTitle,
+      // [Optional] "profileImage:"URL" //String
+    };
+
+    console.log(newProfile);
+
+    axios
+      .put(`/api/v1/users/:userId/userprofile`, newProfile)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <div className="MypageEdit_UserInfoEdit_Container">
       <div className="MypageEdit_UserInfoEdit_Text_Container">
@@ -66,17 +93,34 @@ const UserInfoEdit = () => {
         </div>
         <div className="MypageEdit_UserInfoEdit_FormName_Container">
           <div className="UserInfo_Edit_Subtitle">Display name</div>
-          <Input className="UserInfo_Edit_Input" />
+          <Input
+            className="UserInfo_Edit_Input"
+            value={newName}
+            onChange={changeDisplayName}
+          />
         </div>
         <div className="MypageEdit_UserInfoEdit_FormTitle_Container">
           <div className="UserInfo_Edit_Subtitle">Title</div>
-          <Input className="UserInfo_Edit_Input" />
+          <Input
+            className="UserInfo_Edit_Input"
+            value={newTitle}
+            onChange={changeTitle}
+          />
         </div>
         <div className="MypageEdit_UserInfoEdit_FormButton_Container">
-          <BlueButton width="90px" height="35px">
+          <BlueButton
+            width="90px"
+            height="35px"
+            onClick={changeUserProfile}
+            href={"/users/mypage/:userId"}
+          >
             프로필 저장
           </BlueButton>
-          <BlueWhiteButton width="90px" height="35px">
+          <BlueWhiteButton
+            width="90px"
+            height="35px"
+            href={"/users/mypage/:userId"}
+          >
             취소
           </BlueWhiteButton>
         </div>
