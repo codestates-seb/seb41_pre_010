fde9f26971beb4078eaf251294b5a278eeecf966
sktjgudals <sktjgudals@naver.com>
2022-12-27 14:47:26 +0900

    [FE Refactor] 컴포넌트 분리

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,8 +1,59 @@
 import { useState, useCallback } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, redirect } from "react-router-dom";
+import axios from "axios";
 import Input from "../Components/Input";
 import { BlueButton } from "../Components/Button";
 import "./Styles/SignUp.css";
+
+export default function SignUp() {
+  const url = "http://localhost:8080/api/v1/users/signup";
+  const navigate = useNavigate();
+  const onSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      navigate(-1);
+      // await axios
+      //   .post(url, {
+      //     username: name,
+      //     password: password,
+      //     email: email,
+      //   })
+      //   .then((res) => {
+      //     console.log("response:", res);
+      //     if (res.status === 200) {
+      //     }
+      //   }).catch((e)=>if(e) return redirect("/users/signup"))
+    },
+    [navigate]
+  );
+
+  return (
+    <>
+      <main>
+        <div className="SignUp_Container">
+          <Logo />
+          <div className="SignUp_Content">
+            <form onSubmit={onSubmit}>
+              <NickNameComponent />
+              <EmailComponent />
+              <PasswordComponent />
+              <div className="SignUp_Button_Container">
+                <BlueButton
+                  className="SignUp_Button"
+                  height="30"
+                  fontSize="16px"
+                >
+                  Sign Up
+                </BlueButton>
+              </div>
+            </form>
+          </div>
+          <MessageComponent />
+        </div>
+      </main>
+    </>
+  );
+}
 
 const Logo = () => {
   return (
@@ -19,7 +70,33 @@ const Logo = () => {
   );
 };
 
-export default function SignUp() {
+const NickNameComponent = () => {
+  return (
+    <div className="SignUp_Display_Name_Container">
+      <label for="SignUp_DisplayName" className="SignUp_Content_Title">
+        닉네임
+      </label>
+      <Input
+        id="SignUp_DisplayName"
+        type="text"
+        className="SignUp_Content_Input"
+      />
+    </div>
+  );
+};
+
+const EmailComponent = () => {
+  return (
+    <div className="SignUp_Email_Container">
+      <label for="SignUp_email" className="SignUp_Content_Title">
+        이메일
+      </label>
+      <Input id="SignUp_email" type="text" className="SignUp_Content_Input" />
+    </div>
+  );
+};
+
+const PasswordComponent = () => {
   const [passwordMessage, setPasswordMessage] = useState(
     "비밀번호는 최소 1개의 문자와 1개의 숫자를 포함하여 최소 8자이상이어야 합니다."
   );
@@ -43,63 +120,32 @@ export default function SignUp() {
   }, []);
 
   return (
-    <>
-      <main>
-        <div className="SignUp_Container">
-          <Logo />
-          <div className="SignUp_Content">
-            <form>
-              <div className="SignUp_Display_Name_Container">
-                <label
-                  for="signup_display_name"
-                  className="SignUp_Content_Title"
-                >
-                  닉네임
-                </label>
-                <Input
-                  id="signup_display_name"
-                  type="text"
-                  className="SignUp_Content_Input"
-                />
-              </div>
-              <div className="SignUp_Email_Container">
-                <label className="SignUp_Content_Title">이메일</label>
-                <Input type="text" className="SignUp_Content_Input" />
-              </div>
-              <div className="SignUp_Password_Container">
-                <label className="SignUp_Content_Title">비밀번호</label>
-                <Input
-                  type="password"
-                  className="SignUp_Content_Input"
-                  onChange={onChangePassword}
-                />
-                <span className="SignUp_Password_Message">
-                  {passwordMessage}
-                </span>
-              </div>
-              <div className="SignUp_Button_Container">
-                <BlueButton
-                  className="SignUp_Button"
-                  height="30"
-                  fontSize="16px"
-                >
-                  Sign Up
-                </BlueButton>
-              </div>
-            </form>
-          </div>
-          <div className="SignUp_Accout_Have_Message_Container">
-            <span className="SignUp_Accout_Have_Message">
-              이미 계정이 있으신가요?
-            </span>
-            <div>
-              <Link className="SignUp_Link" to={"/users/login"}>
-                로그인
-              </Link>
-            </div>
-          </div>
-        </div>
-      </main>
-    </>
+    <div className="SignUp_Password_Container">
+      <label for="SignUp_Password" className="SignUp_Content_Title">
+        비밀번호
+      </label>
+      <Input
+        id="SignUp_Password"
+        type="password"
+        className="SignUp_Content_Input"
+        onChange={onChangePassword}
+      />
+      <span className="SignUp_Password_Message">{passwordMessage}</span>
+    </div>
   );
-}
+};
+
+const MessageComponent = () => {
+  return (
+    <div className="SignUp_Accout_Have_Message_Container">
+      <span className="SignUp_Accout_Have_Message">
+        이미 계정이 있으신가요?
+      </span>
+      <div>
+        <Link className="SignUp_Link" to={"/users/login"}>
+          로그인
+        </Link>
+      </div>
+    </div>
+  );
+};
