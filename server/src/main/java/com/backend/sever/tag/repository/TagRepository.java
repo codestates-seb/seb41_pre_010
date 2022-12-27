c483f9bb0d350aceb3a94c7544022935d0059f41
SungCheon Ju <jution113@gmail.com>
2022-12-27 13:20:51 +0900

    [BE Feat] TagRepository에 native query 추가

--- server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
+++ server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
@@ -2,6 +2,11 @@ package com.backend.sever.tag.repository;
 
 import com.backend.sever.tag.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    @Query(value = "SELECT * FROM TAG WHERE TAG_NAME = :tagName", nativeQuery =true)
+    Optional<Tag> findByTagName(String tagName);
 }
