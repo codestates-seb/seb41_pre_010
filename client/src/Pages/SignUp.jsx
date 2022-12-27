6e398add2a2bdf462ecd99a8da45c32e2ef6dfba
sktjgudals <sktjgudals@naver.com>
2022-12-27 13:19:14 +0900

    [FE Feat] 기본 css 변경 및 비밀번호 유효성 검사 적용

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,36 +1,76 @@
-import React from "react";
+import { useState, useCallback } from "react";
+import { Link } from "react-router-dom";
+import Input from "../Components/Input";
+import { BlueButton } from "../Components/Button";
 import "./Styles/SignUp.css";
 
 export default function SignUp() {
+  const [passwordMessage, setPasswordMessage] = useState(
+    "비밀번호는 최소 1개의 문자와 1개의 숫자를 포함하여 최소 8자이상이어야 합니다."
+  );
+  const [isPassword, setIsPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const onChangePassword = useCallback((e) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+    const passwordCurrent = e.target.value;
+    setPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage(
+        "비밀번호는 최소 1개의 문자와 1개의 숫자를 포함하여 최소 8자이상이어야 합니다."
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("");
+      setIsPassword(true);
+    }
+  }, []);
+
   return (
     <>
       <main>
         <div className="SignUp_Container">
           <div className="SignUp_Content">
-            <div className="User_Info_Enter_Container">
-              <div>
-                <div className="Display_Name_Container">
-                  <span>닉네임</span>
-                  <input></input>
-                </div>
-                <div className="Email_Container">
-                  <span>이메일</span>
-                  <input></input>
-                </div>
-                <div className="Password_Container">
-                  <span>비밀번호</span>
-                  <input></input>
-                  <span>
-                    비밀번호는 최소 1개의 문자와 1개의 숫자를 포함하여 최소 8자
-                    이상이어야 합니다.
-                  </span>
-                </div>
+            <div>
+              <div className="SignUp_Display_Name_Container">
+                <label
+                  for="signup_display_name"
+                  className="SignUp_Display_Name"
+                >
+                  닉네임
+                </label>
+                <Input
+                  id="signup_display_name"
+                  type="text"
+                  className="SignUp_Display_Input"
+                />
               </div>
-              <button className="SignUp_Button">Sign Up</button>
+              <div className="SignUp_Email_Container">
+                <span>이메일</span>
+                <Input type="text" className="SignUp_Email_Input" />
+              </div>
+              <div className="SignUp_Password_Container">
+                <span>비밀번호</span>
+                <Input
+                  type="password"
+                  className="SignUp_Password_Input "
+                  onChange={onChangePassword}
+                />
+                <span className="SignUp_Password_Message">
+                  {passwordMessage}
+                </span>
+              </div>
+              <div className="SignUp_Button_Container">
+                <BlueButton className="SignUp_Button" height="30">
+                  Sign Up
+                </BlueButton>
+              </div>
             </div>
           </div>
-          <span className="Accout_Have_Message">
-            이미 계정이 있으신가요? Log in
+          <span className="SignUp_Accout_Have_Message">
+            이미 계정이 있으신가요?
+            <Link to={"/users/login"}>Log in</Link>
           </span>
         </div>
       </main>
