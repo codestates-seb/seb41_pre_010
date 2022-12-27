e09bf77f775545e55b7dd1aaf26ecef9037c7aeb
HanSungHyeon <hsh5349@naver.com>
2022-12-27 11:43:03 +0900

    [BE REFACTOR] 공백 제거 및 안쓰는 부분 제거

--- server/src/main/java/com/backend/sever/vote/VoteController.java
+++ server/src/main/java/com/backend/sever/vote/VoteController.java
@@ -1,7 +1,7 @@
 package com.backend.sever.vote;
 
 import com.backend.sever.vote.dto.VotePostDto;
-import com.backend.sever.vote.mapper.VoteMapper;
+import com.backend.sever.vote.entity.QuestionVote;
 import com.backend.sever.vote.service.VoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,22 +11,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/votes")
 public class VoteController {
     private final VoteService voteService;
-    private final VoteMapper voteMapper;
 
-    public VoteController(VoteService voteService, VoteMapper voteMapper) {
+    public VoteController(VoteService voteService) {
         this.voteService = voteService;
-        this.voteMapper = voteMapper;
     }
 
     @PutMapping("/{question-id}/questions")
     public ResponseEntity putQuestionVote(@RequestBody VotePostDto votePostDto,
                                           @PathVariable ("question-id") long questionId) {
-        voteService.questionVoteUp(voteMapper.voteQuestionPutDtoToVote(votePostDto));
-        return new ResponseEntity(HttpStatus.OK);
-    }
-
-    @GetMapping()
-    public ResponseEntity getTest() {
+        votePostDto.setQuestionId(questionId);
+        QuestionVote questionVote = voteService.upAndDown(votePostDto);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
