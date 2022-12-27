968eb7be7ff69c41231dc67461b04f83fd15c98f
HanSungHyeon <hsh5349@naver.com>
2022-12-27 15:31:02 +0900

    [BE MISC] 매핑 추가

--- server/src/main/java/com/backend/sever/vote/mapper/VoteMapper.java
+++ server/src/main/java/com/backend/sever/vote/mapper/VoteMapper.java
@@ -14,5 +14,6 @@ public interface VoteMapper {
     QuestionVote voteQuestionPutDtoToVote (QuestionVotePutDto questionVotePutDto);
 
     @Mapping(source = "userId",target = "user.userId")
+    @Mapping(source = "answerId",target = "answer.answerId")
     AnswerVote voteAnswerPutDtoToVote(AnswerVotePutDto answerVotePutDto);
 }
