516b2f8eda30f2d9bf80d3b70c26b55880f9cd90
SungCheon Ju <jution113@gmail.com>
2022-12-28 10:27:01 +0900

    [BE Refactor] QuestionMapper 내부 로직 변경

--- server/src/main/java/com/backend/sever/question/mapper/QuestionMapper.java
+++ server/src/main/java/com/backend/sever/question/mapper/QuestionMapper.java
@@ -4,11 +4,79 @@ import com.backend.sever.question.dto.QuestionPostDto;
 import com.backend.sever.question.dto.QuestionPutDto;
 import com.backend.sever.question.dto.QuestionResponseDto;
 import com.backend.sever.question.entity.Question;
+import com.backend.sever.questionTag.dto.QuestionTagResponseDto;
+import com.backend.sever.questionTag.entity.QuestionTag;
+import com.backend.sever.tag.entity.Tag;
 import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface QuestionMapper {
-    Question questionPostDtoToQuestion(QuestionPostDto questionPostDto);
-    Question questionPutDtoToQuestion(QuestionPutDto questionPutDto);
-    QuestionResponseDto questionToQuestionResponseDto(Question question);
+    default Question questionPostDtoToQuestion(QuestionPostDto questionPostDto) {
+        Question question = new Question();
+        question.setTitle(questionPostDto.getTitle());
+        question.setBody(questionPostDto.getBody());
+
+        List<QuestionTag> questionTags = questionPostDto.getTags().stream()
+                .map(questionTagDto -> {
+                    QuestionTag questionTag = new QuestionTag();
+                    Tag tag = new Tag();
+                    tag.setTagId(questionTagDto.getTagId());
+                    questionTag.addQuestion(question);
+                    questionTag.addTag(tag);
+
+                    return questionTag;
+                }).collect(Collectors.toList());
+
+        question.setQuestionTags(questionTags);
+
+        return question;
+    }
+
+    default Question questionPutDtoToQuestion(QuestionPutDto questionPutDto) {
+        Question question = new Question();
+        question.setTitle(questionPutDto.getTitle());
+        question.setBody(questionPutDto.getBody());
+
+        List<QuestionTag> questionTags = questionPutDto.getTags().stream()
+                .map(questionTagDto -> {
+                    QuestionTag questionTag = new QuestionTag();
+                    Tag tag = new Tag();
+                    tag.setTagId(questionTagDto.getTagId());
+                    questionTag.addQuestion(question);
+                    questionTag.addTag(tag);
+
+                    return questionTag;
+                }).collect(Collectors.toList());
+
+        question.setQuestionTags(questionTags);
+
+        return question;
+    }
+
+    default QuestionResponseDto questionToQuestionResponseDto(Question question) {
+        List<QuestionTag> questionTags = question.getQuestionTags();
+
+        QuestionResponseDto questionResponseDto = new QuestionResponseDto();
+        questionResponseDto.setQuestionId(question.getQuestionId());
+        questionResponseDto.setTitle(question.getTitle());
+        questionResponseDto.setBody(question.getBody());
+        questionResponseDto.setTags(questionTagsToQuestionTagResponseDtos(questionTags));
+
+        return questionResponseDto;
+    }
+
+    default List<QuestionTagResponseDto> questionTagsToQuestionTagResponseDtos(List<QuestionTag> questionTags) {
+        return questionTags
+                .stream()
+                .map(questionTag -> QuestionTagResponseDto
+                        .builder()
+                        .tagId(questionTag.getTag().getTagId())
+                        .tagName(questionTag.getTag().getTagName())
+                        .build())
+                .collect(Collectors.toList());
+
+    }
 }
