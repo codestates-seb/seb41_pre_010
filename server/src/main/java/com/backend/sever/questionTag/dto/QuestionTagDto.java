a6ccf9cdc7d11237b078106e02ea22ec26e2ec3e
SungCheon Ju <jution113@gmail.com>
2022-12-28 10:30:09 +0900

    [BE Refactor] QuestionTagDto 내부 필드 변경

--- server/src/main/java/com/backend/sever/questionTag/dto/QuestionTagDto.java
+++ server/src/main/java/com/backend/sever/questionTag/dto/QuestionTagDto.java
@@ -1,2 +1,12 @@
-package com.backend.sever.questionTag.dto;public class QuestionTagDto {
+package com.backend.sever.questionTag.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class QuestionTagDto {
+    private long tagId;
 }
