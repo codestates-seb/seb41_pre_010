b68117c161ce59857714df7c56a21809d480bee6
SungCheon Ju <jution113@gmail.com>
2022-12-28 14:51:10 +0900

    [BE Feat] WebConfig 추가

--- server/src/main/java/com/backend/sever/config/WebConfig.java
+++ server/src/main/java/com/backend/sever/config/WebConfig.java
@@ -1,2 +1,15 @@
-package com.backend.sever.config;public class WebConfig {
+package com.backend.sever.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE", "HEAD", "OPTIONS");
+    }
 }
