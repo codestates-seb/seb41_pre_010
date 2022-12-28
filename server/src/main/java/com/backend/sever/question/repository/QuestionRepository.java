5bf9d07e277268d79dffaac98b3dd137195f9ed6
SungCheon Ju <jution113@gmail.com>
2022-12-28 10:27:59 +0900

    [BE Feat] QuestionRepositoory NativeQuery 추가

--- server/src/main/java/com/backend/sever/question/repository/QuestionRepository.java
+++ server/src/main/java/com/backend/sever/question/repository/QuestionRepository.java
@@ -2,6 +2,18 @@ package com.backend.sever.question.repository;
 
 import com.backend.sever.question.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+    @Query(value = "SELECT QUESTION_TAG_ID FROM QUESTION_TAG WHERE QUESTION_ID = :questionId", nativeQuery =true)
+    List<Long> findQuestionTagIdByQuestionId(Long questionId);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM QUESTION_TAG WHERE QUESTION_TAG_ID = :questionTagId", nativeQuery =true)
+    void deleteQuestionTagIdById(Long questionTagId);
 }
