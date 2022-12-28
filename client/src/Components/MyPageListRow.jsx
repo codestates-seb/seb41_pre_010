31611e1c73340c01abdb149aa87b3dd3807d86c6
HJeong1200 <hjeong1200@gmail.com>
2022-12-28 21:46:58 +0900

    [FE Style] 마이페이지 스타일 수정

--- client/src/Components/MyPageListRow.jsx
+++ client/src/Components/MyPageListRow.jsx
@@ -1,17 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MyPageListRow(props) {
   const name = Object.keys(props)[0];
   const listData = props[name][name];
 
   return (
-    <div className="Mypage_List_Row">
-      {listData.map((el) => {
+    <div className="Mypage_List_Content_Container">
+      {listData.slice(-10).map((el) => {
         const keys = Object.keys(el);
         return (
-          <ul key={el[keys[0]]}>
-            <li>{el[keys[1]]}</li>
-          </ul>
+          <div key={el[keys[0]]} className="Mypage_Content_Container">
+            <span className="Mypage_Content_Votes">{el[keys[4]]}</span>
+            <Link to="" className="Mypage_Content_Title">
+              {el[keys[1]].length > 50
+                ? el[keys[1]].slice(0, 50) + "..."
+                : el[keys[1]]}
+            </Link>
+          </div>
         );
       })}
     </div>
