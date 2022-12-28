34374ec6f71d960f38e78ebbdf13936fcce436ff
SungCheon Ju <jution113@gmail.com>
2022-12-28 10:25:39 +0900

    [BE Refactor] QuestionResponseDto 내부필드 변경

--- server/src/main/java/com/backend/sever/question/dto/QuestionResponseDto.java
+++ server/src/main/java/com/backend/sever/question/dto/QuestionResponseDto.java
@@ -1,25 +1,18 @@
 package com.backend.sever.question.dto;
 
-import com.backend.sever.tag.dto.TagResponseDto;
+import com.backend.sever.questionTag.dto.QuestionTagResponseDto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class QuestionResponseDto {
+    private long questionId;
     private String title;
     private String body;
-    private List<TagResponseDto> tags = createStub();
-
-    private List<TagResponseDto> createStub() {
-        TagResponseDto testTag = new TagResponseDto(0, "test");
-        List<TagResponseDto> stubTagList = new ArrayList<>();
-        stubTagList.add(testTag);
-        return stubTagList;
-    }
+    private List<QuestionTagResponseDto> tags;
 }
