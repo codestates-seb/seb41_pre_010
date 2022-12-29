1736626f94cc9fee7092e3c413f7003f7602e67b
SungCheon Ju <jution113@gmail.com>
2022-12-29 17:07:36 +0900

    [BE Feat] QuestionController에 페이지 검색 로직 구현

--- server/src/main/java/com/backend/sever/question/controller/QuestionController.java
+++ server/src/main/java/com/backend/sever/question/controller/QuestionController.java
@@ -5,9 +5,13 @@ import com.backend.sever.question.dto.QuestionPutDto;
 import com.backend.sever.question.entity.Question;
 import com.backend.sever.question.mapper.QuestionMapper;
 import com.backend.sever.question.service.QuestionService;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/questions")
@@ -34,6 +38,23 @@ public class QuestionController {
         return new ResponseEntity<> (mapper.questionToQuestionResponseDto(question), HttpStatus.OK);
     }
 
+    @GetMapping("/search")
+    public ResponseEntity searchQuestion(@RequestParam("q") Optional<String> keyword,
+                                         @RequestParam("tab") String filter,
+                                         @RequestParam("page") int pageIndex,
+                                         @RequestParam("pageSize") int pageSize) {
+
+        Page<Question> questionPage;
+
+        if(keyword.isPresent()) {
+            questionPage = questionService.findAllQuestions(pageIndex - 1, pageSize, filter, keyword.get());
+        } else {
+            questionPage = questionService.findAllQuestions(pageIndex - 1, pageSize, filter);
+        }
+
+         return new ResponseEntity<>(mapper.questionInfosToPageResponseDto(questionPage), HttpStatus.OK);
+    }
+
     @PutMapping("/{question-id}")
     public ResponseEntity putQuestion(@PathVariable("question-id") long questionId,
                                       @RequestBody QuestionPutDto questionPutDto) {
@@ -49,15 +70,6 @@ public class QuestionController {
         questionService.deleteQuestion(questionId);
 
         return new ResponseEntity<> (HttpStatus.OK);
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity searchQuestion(@RequestParam("q") String searchResult,
-                                         @RequestParam("tab") String tabName,
-                                         @RequestParam("page") int pageNumber,
-                                         @RequestParam("pageSize") int pageSizeNumber) {
-
-        return new ResponseEntity(HttpStatus.OK);
     }
 
 }
