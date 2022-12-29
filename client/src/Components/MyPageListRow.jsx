55f171deb628f085388d7ea9e3845c5748b80c65
HJeong1200 <hjeong1200@gmail.com>
2022-12-29 11:00:19 +0900

    [FE Fix] 한글과 영문에 따라 타이틀 항목 글자수가 다르게 표시되도록 수정

--- client/src/Components/MyPageListRow.jsx
+++ client/src/Components/MyPageListRow.jsx
@@ -7,19 +7,20 @@ function MyPageListRow(props) {
 
   return (
     <div className="Mypage_List_Content_Container">
-      {listData.slice(-10).map((el) => {
+      {listData.slice(-5).map((el) => {
         const [id, title, createdAt, modifiedAt, vote] = Object.keys(el);
+        const displayTitle = el[title].slice(0, 35);
         return (
           <div key={el[id]} className="Mypage_Content_Container">
             <span className="Mypage_Content_Votes">{el[vote]}</span>
-            <Link to="" className="Mypage_Content_Title">
-              {el[title].split("").every((el) => el.charCodeAt() < 127)
+            <Link to={`/questions/${el[id]}`} className="Mypage_Content_Title">
+              {displayTitle.split("").every((el) => el.charCodeAt() < 127)
                 ? el[title].length > 35
-                  ? el[title].slice(0, 35) + "..."
-                  : el[title]
+                  ? displayTitle + "..."
+                  : displayTitle
                 : el[title].length > 20
-                ? el[title].slice(0, 20) + "..."
-                : el[title]}
+                ? displayTitle.slice(0, 20) + "..."
+                : displayTitle}
             </Link>
             <span className="Mypage_Content_Date">
               {el[modifiedAt] || el[createdAt]}
