5830c83017fbb9b5ac1863a55b69832d81583d71
limdumb <dlaruddls@nate.com>
2022-12-29 11:11:04 +0900

    [FE MISC] Vote Function 분리

--- client/src/Pages/Question.jsx
+++ client/src/Pages/Question.jsx
@@ -84,27 +84,6 @@ export default function Question() {
         console.log(err);
       });
   };
-
-  //Vote Request
-  const questionUpVoteRequest = (userId, questionId) => {
-    const request = { userId: userId, questionId: questionId, vote: 1 };
-    axios.put(`api/v1/votes/${questionId}/questions`, JSON.stringify(request));
-  };
-
-  const questionDownVoteRequest = (userId, questionId) => {
-    const request = { userId: userId, questionId: questionId, vote: -1 };
-    axios.put(`api/v1/votes/${questionId}/questions`, JSON.stringify(request));
-  };
-
-  const answerUpVoteRequest = (userId, answerId) => {
-    const request = { userId: userId, questionId: answerId, vote: 1 };
-    axios.put(`api/v1/votes/${answerId}/questions`, JSON.stringify(request));
-  };
-
-  const answerDownVoteRequest = (userId, answerId) => {
-    const request = { userId: userId, questionId: answerId, vote: -1 };
-    axios.put(`api/v1/votes/${answerId}/questions`, JSON.stringify(request));
-  };
   //실제 API 정보에서 수정 예정
   // useEffect(() => {
   //   const getQuestionData = async () => {
