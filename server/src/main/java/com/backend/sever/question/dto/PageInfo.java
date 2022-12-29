c3644046cb9ac33ac36f79876a1e81465ca73eca
SungCheon Ju <jution113@gmail.com>
2022-12-29 11:19:30 +0900

    [BE Feat] PageInfo 추가

--- server/src/main/java/com/backend/sever/question/dto/PageInfo.java
+++ server/src/main/java/com/backend/sever/question/dto/PageInfo.java
@@ -1,2 +1,27 @@
-package com.backend.sever.question.dto;public class PageInfo {
+package com.backend.sever.question.dto;
+
+import com.backend.sever.tag.entity.Tag;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class PageInfo {
+    private long questionId;
+    private UserDto user;
+    private String title;
+    private String body;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+    private int vote;
+    private int answers;
+    private Tag tag;
+
+    static class UserDto {
+        private long userId;
+        private String displayName;
+        private String profileImage;
+    }
 }
