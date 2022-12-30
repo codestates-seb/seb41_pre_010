ff8c014fa50db03dfa2a9c7dadbf92a63b74798c
HJeong1200 <hjeong1200@gmail.com>
2022-12-30 18:00:11 +0900

    [FE Feat] 답변 수정 페이지 생성

---
+++ client/src/Pages/AnswerEditPage.jsx
@@ -0,0 +1,133 @@
+import Input from "../Components/Input";
+import { BlueButton } from "../Components/Button";
+import "./Styles/EditPage.css";
+import TextEditor from "../Components/TextEditor";
+import { useEffect, useState } from "react";
+import { useSession } from "../CustomHook/SessionProvider";
+import axios from "axios";
+import { TagBar } from "../Components/TagBar";
+import { useParams } from "react-router-dom";
+
+const EditTitle = ({ setQuestionTitle, questionTitle }) => {
+  function changeQuestionTitle(e) {
+    setQuestionTitle(e.target.value);
+  }
+
+  return (
+    <div className="Edit_Title_Container">
+      <label htmlFor="EditPage_Title" className="Title">
+        Title
+      </label>
+      <Input
+        id="EditPage_Title"
+        className="Title_Input"
+        onChange={changeQuestionTitle}
+        defaultValue={questionTitle}
+      />
+    </div>
+  );
+};
+
+const AnswserEditPage = () => {
+  const [questionTitle, setQuestionTitle] = useState("");
+  const [questionBodyMD, setQuestionBodyMD] = useState("");
+  const [questionBodyHTML, setQuestionBodyHTML] = useState("");
+  const [selected, setSelected] = useState([]);
+  const { session } = useSession();
+
+  const { questionId } = useParams();
+
+  useEffect(() => {
+    async function loadQuestionContents() {
+      axios
+        .get(
+          `https://359b-112-144-75-111.jp.ngrok.io/api/v1/questions/${questionId}/edit`,
+          {
+            headers: {
+              "ngrok-skip-browser-warning": "69420",
+            },
+          }
+        )
+        .then((res) => {
+          const { title, body, tags } = res.data;
+          const tagNameArr = [];
+
+          for (let el of tags) {
+            tagNameArr.push(el.tagName);
+          }
+
+          setQuestionTitle(title);
+          setQuestionBodyMD(body);
+          setSelected(tagNameArr);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    loadQuestionContents();
+  }, [questionId]);
+
+  function submitEditQuestion() {
+    const tags = { tags: selected };
+
+    axios
+      .post("https://359b-112-144-75-111.jp.ngrok.io/api/v1/tags", tags)
+      .then((res) => {
+        const body = {
+          userId: session.userId,
+          title: questionTitle,
+          body: questionBodyMD,
+          bodyString: questionBodyHTML.replace(/<[^>]+>/g, " ").trim(),
+          tags: res.data.tags,
+        };
+        axios
+          .put(
+            `https://359b-112-144-75-111.jp.ngrok.io/api/v1/questions/${questionId}`,
+            body
+          )
+          .then((res) => {
+            console.log(res);
+            console.log(body);
+          })
+          .catch((err) => console.log(err));
+      });
+  }
+
+  return (
+    <div className="EditPage_Container">
+      <div className="Edit_Container">
+        <div className="EditPage_Ask_Question_Container">
+          <h1 className="EditPage_Ask_Title">Edit your question</h1>
+        </div>
+        <EditTitle
+          setQuestionTitle={setQuestionTitle}
+          questionTitle={questionTitle}
+        />
+        <div className="Body_Container">
+          <label className="Title">Body</label>
+          <TextEditor
+            setQuestionBodyHTML={setQuestionBodyHTML}
+            setQuestionBodyMD={setQuestionBodyMD}
+            initData={questionBodyMD}
+          />
+        </div>
+        <div className="Tags_Container">
+          <span className="Title">Tags</span>
+          <TagBar
+            selected={selected}
+            setSelected={setSelected}
+            className="EditPage_TagBar"
+          />
+        </div>
+        <div className="Buttons_Container">
+          <BlueButton height="36px" onClick={submitEditQuestion}>
+            Review your question
+          </BlueButton>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnswserEditPage;
