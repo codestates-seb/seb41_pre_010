fd4d9b1a8452ad969695f5ecf378d46925cf9ab8
HJeong1200 <hjeong1200@gmail.com>
2022-12-30 13:09:22 +0900

    [FE Feat] EditPage 초기 설정 및 변수명 변경

--- client/src/Pages/EditPage.jsx
+++ client/src/Pages/EditPage.jsx
@@ -1,36 +1,106 @@
-import React from "react";
+import Input from "../Components/Input";
+import { BlueButton } from "../Components/Button";
 import "./Styles/EditPage.css";
+import TextEditor from "../Components/TextEditor";
+import { useState } from "react";
+import { useSession } from "../CustomHook/SessionProvider";
+import axios from "axios";
+import { TagBar } from "../Components/TagBar";
 
-function EditPage() {
+const EditTitle = ({ setQuestionTitle }) => {
+  function changeQuestionTitle(e) {
+    setQuestionTitle(e.target.value);
+  }
+
+  return (
+    <div className="Edit_Title_Container">
+      <label htmlFor="EditPage_Title" className="Title">
+        Title
+      </label>
+      <Input
+        id="EditPage_Title"
+        className="Title_Input"
+        onChange={changeQuestionTitle}
+      />
+    </div>
+  );
+};
+
+const EditPage = () => {
+  const [questionTitle, setQuestionTitle] = useState("");
+  const [questionBodyMD, setQuestionBodyMD] = useState("");
+  const [questionBodyHTML, setQuestionBodyHTML] = useState("");
+  const [selected, setSelected] = useState([]);
+  const [tagsResponse, settagsResponse] = useState([]);
+  const { session } = useSession();
+
+  function submitQuestion() {
+    const tags = { tags: selected };
+    const body = {
+      userId: session.userId,
+      title: questionTitle,
+      body: questionBodyMD,
+      bodyString: questionBodyHTML.replace(/<[^>]+>/g, " "),
+      tags: tagsResponse,
+    };
+
+    axios
+      .post("api/v1/tags", tags)
+      .then((res) => {
+        settagsResponse(res);
+      })
+      .then(() => {
+        axios
+          .post("api/v1/questions", body)
+          .then((res) => {
+            console.log(res);
+            console.log(body);
+          })
+          .catch((err) => console.log(err));
+      })
+      .finally(() => {
+        axios
+          .post("api/v1/questions", body)
+          .then((res) => {
+            console.log(res);
+          })
+          .catch((err) => {
+            console.log(err);
+            console.log(body);
+          });
+      });
+  }
+
   return (
     <div className="EditPage_Container">
       <div className="Edit_Container">
-        <div className="Edit_Title_Container">
-          <span className="Title">Title</span>
-          <input className="Title_Input" />
+        <div className="EditPage_Ask_Question_Container">
+          <h1 className="EditPage_Ask_Title">Edit your question</h1>
         </div>
+        <EditTitle setQuestionTitle={setQuestionTitle} />
         <div className="Body_Container">
-          <span className="Title">Body</span>
-          <textarea className="Edit_Input" />
-        </div>
-        <div className="Preview_Container">
-          <span className="Title">Preview</span>
-          <div className="Preview">
-            {`인풋창에 쓴 내용의 미리보기
-            Preview 컨테이너도 작성한 내용에 따라서 유동적으로 움직일 예정입니다`}
-          </div>
+          <label className="Title">Body</label>
+          <TextEditor
+            setQuestionBodyHTML={setQuestionBodyHTML}
+            setQuestionBodyMD={setQuestionBodyMD}
+          />
         </div>
         <div className="Tags_Container">
           <span className="Title">Tags</span>
-          <input className="Tags_Input" />
+          <TagBar
+            selected={selected}
+            setSelected={setSelected}
+            className="EditPage_TagBar"
+          />
         </div>
         <div className="Buttons_Container">
-          <button className="Save_Edit">Save edits</button>
-          <button className="Cancel_Edit">Cancel</button>
+          <BlueButton height="36px" onClick={submitQuestion}>
+            Review your question
+          </BlueButton>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default EditPage;
