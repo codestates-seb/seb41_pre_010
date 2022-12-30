4367acddbf10ada8c4343d3c1edbf42f622674ba
sktjgudals <sktjgudals@naver.com>
2022-12-30 19:48:55 +0900

    [FE Style]  모달 스타일링 완료

--- client/src/Components/UserProfile/Image.jsx
+++ client/src/Components/UserProfile/Image.jsx
@@ -1,12 +1,11 @@
 import { useState } from "react";
+import { BlueButton } from "../Button";
 import ImageUploading from "react-images-uploading";
 
 import "../Styles/Image.css";
 
 const ImageUpload = ({ userProfileImage, userId }) => {
-  const [images, setImages] = useState([
-    { data_url: userProfileImage, file: "" },
-  ]);
+  const [images, setImages] = useState([]);
   const maxNumber = 1;
 
   const onChange = (imageList, addUpdateIndex) => {
@@ -14,7 +13,7 @@ const ImageUpload = ({ userProfileImage, userId }) => {
   };
 
   return (
-    <div className="App">
+    <>
       <ImageUploading
         multiple
         value={images}
@@ -23,16 +22,7 @@ const ImageUpload = ({ userProfileImage, userId }) => {
         dataURLKey="data_url"
         acceptType={["jpg", "jpeg", "svg", "png"]}
       >
-        {({
-          imageList,
-          onImageUpload,
-          onImageRemoveAll,
-          onImageUpdate,
-          onImageRemove,
-          isDragging,
-          dragProps,
-          errors,
-        }) => (
+        {({ imageList, onImageUpdate, onImageRemove, dragProps, errors }) => (
           <div className="Upload_Image_Container">
             <div>
               <div className="Upload_Image_Modal_DropZone_Container">
@@ -49,33 +39,46 @@ const ImageUpload = ({ userProfileImage, userId }) => {
                     {...dragProps}
                     onClick={() => onImageUpdate(0)}
                   >
-                    <div className="Upload_Image_Text_Container">
-                      <p className="Upload_Image_Text">
-                        <b>Drag and drop or click here</b>
-                        to upload your image (max 2mb)
-                      </p>
+                    <div className="Upload_Image_Modal_Image">
+                      <div className="Upload_Image_Text_Container">
+                        <p className="Upload_Image_Text">
+                          <b>Drag and drop or click here</b>
+                          <p>to upload your image (max 2mb)</p>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 )}
               </div>
+              {imageList[0] && (
+                <>
+                  <div className="Upload_Image_Remove_Container">
+                    <button
+                      onClick={() => {
+                        onImageRemove(0);
+                      }}
+                      className="Upload_Image_Remove_Button"
+                    >
+                      취소하고 다른 이미지 추가하기
+                    </button>
+                  </div>
+                </>
+              )}
             </div>
-            <button
-              onClick={() => {
-                onImageRemove(0);
-              }}
-            >
-              Remove
-            </button>
             {errors && (
-              <div>
+              <div className="Upload_Image_Remove_Container">
                 {errors.acceptType && (
-                  <span>Your selected file type is not allow</span>
+                  <span className="Upload_Image_Error">
+                    Your selected file type is not allow
+                  </span>
                 )}
                 {errors.maxFileSize && (
-                  <span>Selected file size exceed maxFileSize</span>
+                  <span className="Upload_Image_Error">
+                    Selected file size exceed maxFileSize
+                  </span>
                 )}
                 {errors.resolution && (
-                  <span>
+                  <span className="Upload_Image_Error">
                     Selected file is not match your desired resolution
                   </span>
                 )}
@@ -84,7 +87,12 @@ const ImageUpload = ({ userProfileImage, userId }) => {
           </div>
         )}
       </ImageUploading>
-    </div>
+      <div className="UploadImage_Submit_Container">
+        <BlueButton width="130px" height="50px">
+          이미지 등록하기
+        </BlueButton>
+      </div>
+    </>
   );
 };
 export default ImageUpload;
