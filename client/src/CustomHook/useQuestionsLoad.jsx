8db8873c0ab56734408c5185be1ae4c0c7a4657f
HJeong1200 <hjeong1200@gmail.com>
2022-12-30 23:26:00 +0900

    [FE HOTFIX] 메인페이지 전체 질문 로딩 url 수정

--- client/src/CustomHook/useQuestionsLoad.jsx
+++ client/src/CustomHook/useQuestionsLoad.jsx
@@ -13,7 +13,7 @@ function useQuestionsLoad(
   const [totalQuestions, setTotalQuestions] = useState(23338049);
 
   const getSearchValueUrl = `/api/v1/questions/search?q=${searchValue}&tab=${tabName}&page=${pageNumber}&pageSize=${pageSizeNumber}`;
-  const getAllPagesUrl = `ttps://e066-112-144-75-111.jp.ngrok.io/api/v1/questions/search?tab=${tabName}&page=${pageNumber}&pageSize=${pageSizeNumber}`;
+  const getAllPagesUrl = `/api/v1/questions/search?tab=${tabName}&page=${pageNumber}&pageSize=${pageSizeNumber}`;
 
   useEffect(() => {
     async function getQuestionList(tabName, pageNumber, pageSizeNumber) {
