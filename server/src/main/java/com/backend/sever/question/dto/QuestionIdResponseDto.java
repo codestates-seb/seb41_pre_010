41c66baf63567afccfa811765bbe042fc47731b8
SungCheon Ju <jution113@gmail.com>
2023-01-02 14:11:37 +0900

    [BE Feat] QuestionIdResponseDto question/post return question id dto 구현

--- server/src/main/java/com/backend/sever/question/dto/QuestionIdResponseDto.java
+++ server/src/main/java/com/backend/sever/question/dto/QuestionIdResponseDto.java
@@ -1,2 +1,12 @@
-package com.backend.sever.question.dto;public class QuestionIdResponseDto {
+package com.backend.sever.question.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class QuestionIdResponseDto {
+    private long questionId;
 }
