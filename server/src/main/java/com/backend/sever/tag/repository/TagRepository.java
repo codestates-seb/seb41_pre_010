8bbd0a93ea9576eaa081a0437afe316f600f0415
ppk150 <vytpdnd11@naver.com>
2023-01-02 11:56:59 +0900

    [BE FEAT] get 요청 해결 시도 - 5

--- server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
+++ server/src/main/java/com/backend/sever/tag/repository/TagRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
-    @Query(value = "SELECT * FROM TAG WHERE TAG_NAME = :tagName", nativeQuery =true)
+    @Query(value = "SELECT * FROM TESTTAG WHERE TAG_NAME = :tagName", nativeQuery =true)
     Optional<Tag> findByTagName(String tagName);
 
-    @Query(value = "SELECT TAG_ID FROM TAG WHERE TAG_NAME = :tagName", nativeQuery =true)
+    @Query(value = "SELECT TAG_ID FROM TESTTAG WHERE TAG_NAME = :tagName", nativeQuery =true)
     long findTagId(String tagName);
 }
